7363e977e509d7128e7cc839fc632c1c67be32fa
Evita Barboza <151908249+evitabarboza@users.noreply.github.com>
2025-02-04 22:23:58 +0530

    done till third part

--- main.py
+++ main.py
@@ -29,6 +29,24 @@ class Sprite(turtle.Turtle):
     def move(self):
         self.fd(self.speed)
 
+        #Boundary detection
+        if self.xcor() > 290:
+            self.setx(290)
+            self.rt(60)
+
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.rt(60)
+
+        if self.ycor() > 290:
+            self.sety(290)
+            self.rt(60)
+        
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.rt(60)
+
+
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
         Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -47,7 +65,33 @@ class Player(Sprite):
     def decelerate(self):
         self.speed -= 1
 
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
 
+    def draw_border(self):
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)  # Fix: changed size(3) to pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300, 300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+
+
+#Create game objevt
+game = Game()
+
+#Draw the game border
+game.draw_border()
 
 #Create my sprites
 player = Player("triangle", "white", 0, 0)
